589d55d0cdb4f4aec0c3e4b449cafddfbbbb9e42
Huseyin OZDOGAN <huseyinozdogan@gmail.com>
2021-07-14 21:48:49 +0300

    vuex initial commit

---
+++ vuex.vue
@@ -0,0 +1,13 @@
+vue devtools for chrome extension 
+fundamentals of vuex 
+    DTO ya emit diyor 
+props: are custom attributes you can register on a component. when a value is passed to a prop attribute, it becomes a property on that component instance 
+events are used for communication between components as opposed to communication between DOM nodes. 
+vue components makes dispatch to backend API(actions) -> actions commits mutation->mutations mutate state->state renders vue components
+vuex+vue(SFC)
+vuex+vue router
+composing vuex 
+vuex modules 
+vuex plugins 
+normalizing state 
+
